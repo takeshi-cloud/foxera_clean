08b38db028a4da8c418709a3b7d11b772504edb4
takeshi <takeshi@foxera.jp>
2026-03-29 16:54:31 +0900

    use削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-"use client";
+// "use client"; ← これ削除
 
 import HomeClient from "./HomeClient";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
+
 
 export default function Page() {
   return <HomeClient />;
 }
+//a
